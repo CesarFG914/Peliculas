578db89b2972665463e42e5ac0dbd7a6c41891b0
CesarFG914 <fernandezcesar085mail.com>
2025-04-30 23:23:11 -0600

    Creacion de proyecto basado en peliculas Laravel

---
+++ peliculas/resources/views/inicio.blade.php
@@ -0,0 +1,9 @@
+@extends('plantilla')
+
+@section('contenido')
+<div class="text-center mt-5">
+    <img src="{{ asset('img/Blockbuster_logo.svg.png') }}" alt="Blockbuster Logo" style="max-width: 300px;">
+    <h1 class="mt-4">Bienvenido al Catálogo de Películas</h1>
+</div>
+@endsection
+
